26c70555572196bcb3b0aab66c01a42d5c3a4d15
pioet <1599023541@qq.com>
2026-04-30 20:41:49 +0800

    fix: strip [] in the line element when rendering colorrs

--- lib/colors.ts
+++ lib/colors.ts
@@ -1,7 +1,8 @@
 import { lineColors } from "./json-loader";
 
 export function getLineColor(lineName: string): string {
-  return lineColors[lineName] || lineColors["default"] || "#666666";
+  const cleanName = lineName.replace(/\[.*?\]/g, '').replace(/（.*?）/g, '').replace(/\(.*?\)/g, '');
+  return lineColors[cleanName] || lineColors["default"] || "#666666";
 }
 
 export { lineColors };
